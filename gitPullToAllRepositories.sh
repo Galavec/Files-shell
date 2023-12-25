b4a627a4611b5a70e8efcea45489cffc2a78692d
Hector Galarza <galavec@hotmail.com>
2023-12-25 04:46:54 -0500

    [gitPullToAllRepositories] - Muestra repositorios pendientes de commit

--- gitPullToAllRepositories.sh
+++ gitPullToAllRepositories.sh
@@ -1,4 +1,4 @@
-# Version: 2.2.0
+# Version: 3.0.0
 # Nota, se recomienda ejecutarlo desde la consola de "Git Bash" con versión de Bash "4.0".
 
 TOKEN="123"
@@ -26,27 +26,33 @@ for proyecto in */; do
 
     # Verifica si es un repositorio Git validando si el subdirectorio contiene un directorio ".git".
     if [ -d .git ]; then
-        # OUTPUT=$(git -c "http.extraheader=AUTHORIZATION: bearer $TOKEN" pull 2>&1) # GitHub con TOKEN y mediante HTTPS
-        # OUTPUT=$(git pull 2>&1) # GitHub/GitLab usando clave SSH. Vincula la clave en el 'Agente SSH' de tu sistema operativo para que no te la vuelva a solicitar.
-        OUTPUT=$(git -c "http.extraheader=PRIVATE-TOKEN: $TOKEN" pull 2>&1) # TOKEN para GitLab.
-        EXIT_STATUS=$?
+        GIT_STATUS=$(git status 2>&1)
 
-        if [ "$EXIT_STATUS" -ne 0 ]; then
-            if echo "$OUTPUT" | grep -q 'Authentication failed'; then
-                listas["lErrorCredenciales"]+="$proyecto "
-            elif echo "$OUTPUT" | grep -q 'for could not be found'; then
-                listas["lRepositorioNoEncontrado"]+="$proyecto "
-            elif echo "$OUTPUT" | grep -q 'connect to server'; then
-                listas["lErrorRepositorioEnOtroServidor"]+="$proyecto "
-            elif echo "$OUTPUT" | grep -q 'Could not resolve host'; then
-                listas["lErrorPorInternetVpn"]+="$proyecto "
+        if ! echo "$GIT_STATUS" | grep -Eq 'modified:|new file:|deleted:'; then
+            # OUTPUT=$(git -c "http.extraheader=AUTHORIZATION: bearer $TOKEN" pull 2>&1) # GitHub con TOKEN y mediante HTTPS
+            # OUTPUT=$(git pull 2>&1) # GitHub/GitLab usando clave SSH. Vincula la clave en el 'Agente SSH' de tu sistema operativo para que no te la vuelva a solicitar.
+            OUTPUT=$(git -c "http.extraheader=PRIVATE-TOKEN: $TOKEN" pull 2>&1) # TOKEN para GitLab.
+            EXIT_STATUS=$?
+
+            if [ "$EXIT_STATUS" -ne 0 ]; then
+                if echo "$OUTPUT" | grep -q 'Authentication failed'; then
+                    listas["lErrorCredenciales"]+="$proyecto "
+                elif echo "$OUTPUT" | grep -q 'for could not be found'; then
+                    listas["lRepositorioNoEncontrado"]+="$proyecto "
+                elif echo "$OUTPUT" | grep -q 'connect to server'; then
+                    listas["lErrorRepositorioEnOtroServidor"]+="$proyecto "
+                elif echo "$OUTPUT" | grep -q 'Could not resolve host'; then
+                    listas["lErrorPorInternetVpn"]+="$proyecto "
+                else
+                    listas["lRepositorioEnConflictos"]+="$proyecto "
+                fi
+            elif echo "$OUTPUT" | grep -Eq 'Already up to date|files changed|insertions|deletions'; then
+                listas["lRepositorioActualizado"]+="$proyecto "
             else
-                listas["lRepositorioEnConflictos"]+="$proyecto "
+                listas["lErrorDesconocido"]+="$proyecto "
             fi
-        elif echo "$OUTPUT" | grep -Eq 'Already up to date|files changed|insertions|deletions'; then
-            listas["lRepositorioActualizado"]+="$proyecto "
         else
-            listas["lErrorDesconocido"]+="$proyecto "
+            listas["lFaltaCommit"]+="$proyecto "
         fi
     else
         listas["lNoEsRepositorioDeGit"]+="$proyecto "
@@ -56,6 +62,7 @@ for proyecto in */; do
 done
 
 imprimir_list listas[lNoEsRepositorioDeGit] "- Las siguientes carpetas no son repositorios de Git:"
+imprimir_list listas[lFaltaCommit] "* Los siguientes repositorios estan en espera de COMMIT:"
 imprimir_list listas[lErrorCredenciales] "* Repositorios con errores de credenciales:"
 imprimir_list listas[lRepositorioNoEncontrado] "- Repositorios no encontrados:"
 imprimir_list listas[lErrorRepositorioEnOtroServidor] "* Quizás estos repositorios se encuentren en un servidor distinto o en otra cuenta de usuario:"
